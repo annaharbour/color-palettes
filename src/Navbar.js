c0b47cc7e7d6c63e7161bc14d3c76d01a75e0825
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-11-07 10:07:57 -0500

    added select and material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { MenuItem, Select } from '@mui/material';
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css"
 import './Navbar.css'
 
 class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {format: "hex"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        this.setState({format: e.target.value});
+        this.props.handleChange(e.target.value);
+    }
     render() {
-        const {level, changeLevel} = this.props
+        const {level, changeLevel, handleChange} = this.props;
+        const {format} = this.state;
         return (
             <header className='navbar'>
                 <div className='logo'>
@@ -16,6 +27,13 @@ class Navbar extends Component {
                     <div className="slider">
                      <Slider defaultValue={level} min={100} max={900} step={100} onChange={changeLevel} />
                     </div>
+                </div>
+                <div className='select-container'>
+                  <Select value={format} onChange={this.handleChange}>
+                    <MenuItem value="hex">Hex - #ffffff</MenuItem>
+                    <MenuItem value="rgb">Hex - rgb(255,255,255)</MenuItem>
+                    <MenuItem value="rgba">Hex - rgba(255,255,255, 1.0)</MenuItem>
+                  </Select>
                 </div>
             </header>
         );
