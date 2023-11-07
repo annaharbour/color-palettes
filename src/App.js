8113ea1dac0220788e4394d4d7fd90a2e21866ed
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-11-06 19:25:53 -0500

    Add color slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Palette from './Palette';
+import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-             <Palette {...seedColors[4]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
